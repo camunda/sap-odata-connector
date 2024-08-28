e0bfc14a0772a2d75f415a194204ad6b26188b40
Volker Buzek <volker.buzek@camunda.com>
2024-08-28 13:51:30 +0200

    test: validate odata request error

--- src/test/java/io/camunda/sap_integration/ErrorCodesTest.java
+++ src/test/java/io/camunda/sap_integration/ErrorCodesTest.java
@@ -1,13 +1,15 @@
 package io.camunda.sap_integration;
 
+import com.sap.cloud.sdk.cloudplatform.connectivity.AuthenticationType;
+import com.sap.cloud.sdk.cloudplatform.connectivity.DefaultHttpDestination;
+import com.sap.cloud.sdk.cloudplatform.connectivity.DestinationAccessor;
 import com.sap.cloud.sdk.datamodel.odata.client.ODataProtocol;
 import io.camunda.connector.api.error.ConnectorException;
 import io.camunda.connector.test.outbound.OutboundConnectorContextBuilder;
 import io.camunda.sap_integration.model.ErrorCodes;
+import io.vavr.control.Try;
 import org.json.JSONObject;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
@@ -33,6 +35,44 @@ public class ErrorCodesTest {
     assertThat(exception.getErrorCode()).isEqualTo(String.valueOf(ErrorCodes.DESTINATION_ERROR));
   }
 
-//  @Test
-//  void
+  @Nested
+  class odata {
+    @BeforeEach
+      // enable static destination resolution independent of the env var
+    void mockDestination() {
+      DestinationAccessor.setLoader(null);
+      var destination = DefaultHttpDestination.builder("http://localhost:4004")
+          .authenticationType(AuthenticationType.BASIC_AUTHENTICATION)
+          .basicCredentials("alice", "password")
+          .trustAllCertificates()
+          .build();
+      DestinationAccessor.prependDestinationLoader((name, options) -> Try.success(destination));
+    }
+
+    @AfterEach
+    void resetDestination() {
+      DestinationAccessor.setLoader(null);
+    }
+
+    @Test
+    void request_error() {
+      var input = new JSONObject()
+          .put("tpl_Destination", "willalwaysresolve")
+          .put("tpl_HttpMethod", "GET")
+          .put("tpl_ODataService", "/will/cause")
+          .put("tpl_EntityOrEntitySet", "fourohfour")
+          .put("tpl_ODataVersion", ODataProtocol.V4);
+
+      var context = OutboundConnectorContextBuilder.create()
+          .variables(input.toString())
+          .build();
+
+      var function = new SAPconnector();
+
+      ConnectorException exception = assertThrowsExactly(ConnectorException.class, () -> function.execute(context));
+      assertThat(exception.getErrorCode()).isEqualTo(String.valueOf("404"));
+    }
+  }
+
+
 }
