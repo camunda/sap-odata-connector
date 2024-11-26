064cc3432586dc42694533b35d1cc7f2751b3801
Volker Buzek <volker.buzek@camunda.com>
2024-11-26 13:57:11 +0100

    test: prep test-time artifacts
    essentially a copy from connectors-e2e-test-base
    src/main/java/io/camunda/connector/e2e/**/*

---
+++ src/test/java/io/camunda/connector/sap/LocalConnectorRuntimeTest.java
@@ -0,0 +1,13 @@
+package io.camunda.connector.sap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = LocalConnectorRuntime.class)
+public class LocalConnectorRuntimeTest {
+
+  @Test
+  void contextLoads() {
+    // This test will simply start the application context to verify if it loads successfully.
+  }
+}
