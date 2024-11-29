6dd6f63c10793095378872b64b77e4924f1ebb3e
Volker Buzek <volker.buzek@camunda.com>
2024-11-29 17:06:31 +0100

    chore: make env transparent for debugging

--- src/test/java/io/camunda/connector/sap/LocalConnectorRuntime.java
+++ src/test/java/io/camunda/connector/sap/LocalConnectorRuntime.java
@@ -2,9 +2,15 @@ package io.camunda.connector.sap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class LocalConnectorRuntime {
+    private final Environment environment;
+
+    public LocalConnectorRuntime(Environment environment) {
+      this.environment = environment;
+    }
 
   public static void main(String[] args) {
     SpringApplication.run(LocalConnectorRuntime.class, args);
