38d4b31f956316b98d136165cee68ae767add3e1
Volker Buzek <volker.buzek@camunda.com>
2024-12-03 11:34:42 +0100

    test: plain old zeebe api
    don't use zeebe spring test or any other prepped test setup
    but connect plain to c8 saas,
    deploy and run process,
    validate result

--- src/test/java/io/camunda/connector/sap/LocalConnectorRuntimeTest.java
+++ src/test/java/io/camunda/connector/sap/LocalConnectorRuntimeTest.java
@@ -1,36 +1,74 @@
 package io.camunda.connector.sap;
 
-// import static io.camunda.process.test.api.CamundaAssert.assertThat;
-
-import io.camunda.connector.e2e.BpmnFile;
-import io.camunda.connector.e2e.ZeebeTest;
+import com.sap.cloud.sdk.cloudplatform.connectivity.AuthenticationType;
+import com.sap.cloud.sdk.cloudplatform.connectivity.DefaultHttpDestination;
+import com.sap.cloud.sdk.cloudplatform.connectivity.DestinationAccessor;
 import io.camunda.zeebe.client.ZeebeClient;
-import io.camunda.zeebe.process.test.assertions.BpmnAssert;
+import io.vavr.control.Try;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
 import org.springframework.test.context.TestPropertySource;
 
+import java.util.ArrayList;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest(classes = LocalConnectorRuntime.class)
-@TestPropertySource(locations = "classpath:application.properties")
+//@TestPropertySource(locations = "classpath:application.properties")
 public class LocalConnectorRuntimeTest {
 
-  @Autowired Environment env;
+  Environment env;
+  ZeebeClient zeebeClient;
 
-  @Test
-  void contextLoads() {
-    var zeebeClient =
-        ZeebeClient.newCloudClientBuilder()
-            .withClusterId(env.getProperty("camunda.client.cluster-id"))
-            .withClientId(env.getProperty("camunda.client.auth.client-id"))
-            .withClientSecret(env.getProperty("camunda.client.auth.client-secret"))
-            .withRegion(env.getProperty("camunda.client.region"))
+  LocalConnectorRuntimeTest(@Autowired Environment environment) {
+    this.env = environment;
+    zeebeClient = ZeebeClient.newCloudClientBuilder()
+        .withClusterId(env.getProperty("camunda.client.cluster-id"))
+        .withClientId(env.getProperty("camunda.client.auth.client-id"))
+        .withClientSecret(env.getProperty("camunda.client.auth.client-secret"))
+        .withRegion(env.getProperty("camunda.client.region"))
+        .build();
+  }
+
+  @BeforeAll
+  static void mockDestination() {
+    DestinationAccessor.setLoader(null);
+    var destination =
+        DefaultHttpDestination.builder("http://localhost:4004")
+            .authenticationType(AuthenticationType.BASIC_AUTHENTICATION)
+            .basicCredentials("alice", "password")
+            .trustAllCertificates()
             .build();
-    var model = BpmnFile.replace("minimal-mockserver-sample.bpmn");
-    var bpmnTest =
-        ZeebeTest.with(zeebeClient).deploy(model).createInstance().waitForProcessCompletion();
-    var processInstanceEvent = bpmnTest.getProcessInstanceEvent();
-    BpmnAssert.assertThat(processInstanceEvent).hasVariableWithValue("result_get_v4_expr", "42");
+    DestinationAccessor.prependDestinationLoader((name, options) -> Try.success(destination));
+  }
+
+  @SneakyThrows
+  @Test
+    // gets the books from the local bookshop mockserver
+    // via odata v2 + v4
+  void get_v2_v4() {
+    zeebeClient.newDeployResourceCommand()
+        .addResourceFromClasspath("minimal-mockserver-sample.bpmn")
+        .send()
+        .join();
+
+    var processInstanceResult = zeebeClient.newCreateInstanceCommand()
+        .bpmnProcessId("minimal-mockserver-sample")
+        .latestVersion()
+        .withResult()
+        .send()
+        .join();
+
+    ArrayList result_v4 = (ArrayList) processInstanceResult.getVariablesAsMap().get("result_get_v4_expr");
+    assertEquals("Wuthering Heights", ((Map) result_v4.get(0)).get("title"));
+    assertEquals(12, ((Map) result_v4.get(0)).get("stock"));
+    ArrayList result_v2 = (ArrayList) processInstanceResult.getVariablesAsMap().get("result_get_v2_expr");
+    assertEquals("Wuthering Heights", ((Map) result_v2.get(0)).get("title"));
+    assertEquals(12, ((Map) result_v2.get(0)).get("stock"));
   }
 }
